371ec60aacc3ab855d130f1723394753ae6d09df
erjitendra <jitendrashahar@gmail.com>
2017-08-22 23:52:38 +0530

    Drawer&Fragment

--- app/src/main/java/com/example/android/jindalfresh/MainActivity.java
+++ app/src/main/java/com/example/android/jindalfresh/MainActivity.java
@@ -1,13 +1,12 @@
 package com.example.android.jindalfresh;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -29,10 +28,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setToolBar();
-
         fragmentStart();
+        setUpNavigation();
 
+    }
 
+    // ======================Navigation setup  methods==============================
+
+    private void setUpNavigation() {
         navigationView=(NavigationView)findViewById(R.id.naviagation_view);
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -76,7 +79,6 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
-
     }
 
     private void fragmentStart() {
--- app/src/main/java/com/example/android/jindalfresh/navigation/NavigationHelper.java
+++ app/src/main/java/com/example/android/jindalfresh/navigation/NavigationHelper.java
@@ -1,8 +1,6 @@
 package com.example.android.jindalfresh.navigation;
 
-/**
- * Created by Jindal on 8/20/2017.
- */
-
 public class NavigationHelper {
+
+
 }
