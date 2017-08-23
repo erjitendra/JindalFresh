f33940c5e9a5a53b32e93cb31128a52d9c47ebca
erjitendra <jitendrashahar@gmail.com>
2017-08-23 17:39:53 +0530

    SimpleArray

--- app/src/main/java/com/example/android/jindalfresh/HomeFragment.java
+++ app/src/main/java/com/example/android/jindalfresh/HomeFragment.java
@@ -3,6 +3,7 @@ package com.example.android.jindalfresh;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,7 +24,30 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        String[] words = new String[10];
+        words[0] = "one";
+        words[1] = "two";
+        words[2] = "three";
+        words[3] = "four";
+        words[4] = "five";
+        words[5] = "six";
+        words[6] = "seven";
+        words[7] = "eight";
+        words[8] = "nine";
+        words[9] = "ten";
+        Log.v("NumbersActivity", "Word at index 0: " + words[0]);
+        Log.v("NumbersActivity", "Word at index 1: " + words[1]);
+        Log.v("NumbersActivity", "Word at index 2: " + words[2]);
+        Log.v("NumbersActivity", "Word at index 3: " + words[3]);
+        Log.v("NumbersActivity", "Word at index 4: " + words[4]);
+        Log.v("NumbersActivity", "Word at index 5: " + words[5]);
+        Log.v("NumbersActivity", "Word at index 6: " + words[6]);
+        Log.v("NumbersActivity", "Word at index 7: " + words[7]);
+        Log.v("NumbersActivity", "Word at index 8: " + words[8]);
+        Log.v("NumbersActivity", "Word at index 9: " + words[9]);
+        return rootView;
     }
 
 }
