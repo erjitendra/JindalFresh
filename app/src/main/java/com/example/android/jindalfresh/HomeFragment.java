267cf00b2d74389e67b59b696098cb0a4d08bf89
erjitendra <jitendrashahar@gmail.com>
2017-08-23 18:05:21 +0530

    ArrayList

--- app/src/main/java/com/example/android/jindalfresh/HomeFragment.java
+++ app/src/main/java/com/example/android/jindalfresh/HomeFragment.java
@@ -7,6 +7,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
 /**
@@ -26,28 +30,36 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
 
-        String[] words = new String[10];
-        words[0] = "one";
-        words[1] = "two";
-        words[2] = "three";
-        words[3] = "four";
-        words[4] = "five";
-        words[5] = "six";
-        words[6] = "seven";
-        words[7] = "eight";
-        words[8] = "nine";
-        words[9] = "ten";
-        Log.v("NumbersActivity", "Word at index 0: " + words[0]);
-        Log.v("NumbersActivity", "Word at index 1: " + words[1]);
-        Log.v("NumbersActivity", "Word at index 2: " + words[2]);
-        Log.v("NumbersActivity", "Word at index 3: " + words[3]);
-        Log.v("NumbersActivity", "Word at index 4: " + words[4]);
-        Log.v("NumbersActivity", "Word at index 5: " + words[5]);
-        Log.v("NumbersActivity", "Word at index 6: " + words[6]);
-        Log.v("NumbersActivity", "Word at index 7: " + words[7]);
-        Log.v("NumbersActivity", "Word at index 8: " + words[8]);
-        Log.v("NumbersActivity", "Word at index 9: " + words[9]);
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("one");
+        words.add("Two");
+        words.add("Three");
+        words.add("Four");
+        words.add("Five");
+        words.add("Six");
+        words.add("Seven");
+        words.add("Eight");
+        words.add("Nine");
+        words.add("Ten");
+
+        Log.v("NumbersActivity", "Word at index 0: " + words.get(0));
+        Log.v("NumbersActivity", "Word at index 1: " + words.get(1));
+        Log.v("NumbersActivity", "Word at index 2: " + words.get(2));
+        Log.v("NumbersActivity", "Word at index 3: " + words.get(3));
+        Log.v("NumbersActivity", "Word at index 4: " + words.get(4));
+        Log.v("NumbersActivity", "Word at index 5: " + words.get(5));
+        Log.v("NumbersActivity", "Word at index 6: " + words.get(6));
+        Log.v("NumbersActivity", "Word at index 7: " + words.get(7));
+        Log.v("NumbersActivity", "Word at index 8: " + words.get(8));
+        Log.v("NumbersActivity", "Word at index 9: " + words.get(9));
+        LinearLayout LinearLayoutView = (LinearLayout) rootView.findViewById(R.id.Home_LinearLayout_id);
+        for (int index = 0; index < words.size(); index++) {
+            TextView wordView = new TextView(getContext());
+            wordView.setText(words.get(index));
+            LinearLayoutView.addView(wordView);
+        }
         return rootView;
+
     }
 
 }
