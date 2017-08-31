6f9068e2cb4a72592cee19b859ef93651412a276
erjitendra <jitendrashahar@gmail.com>
2017-08-31 23:35:17 +0530

    intermediate: going for api integration

--- app/src/main/java/com/example/android/jindalfresh/MainActivity.java
+++ app/src/main/java/com/example/android/jindalfresh/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.jindalfresh;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
@@ -12,6 +13,8 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,9 +34,13 @@ public class MainActivity extends AppCompatActivity {
         fragmentStart();
         setUpNavigation();
 
-    }
+        String productsUrl = "http://lit-dusk-68336.herokuapp.com/api/v1/product/products/";
 
-    // ======================Navigation setup  methods==============================
+        EarthquakeAsyncTask task = new EarthquakeAsyncTask();
+        task.execute(productsUrl);
+
+
+    }
 
     private void setUpNavigation() {
         navigationView=(NavigationView)findViewById(R.id.naviagation_view);
@@ -81,15 +88,14 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    // ======================Navigation setup  methods==============================
+
     private void fragmentStart() {
         fragmentTransaction=getSupportFragmentManager().beginTransaction();
         fragmentTransaction.add(R.id.frame_layout,new HomeFragment());
         fragmentTransaction.commit();
         getSupportActionBar().setTitle("Home");
     }
-
-
-    // ======================Tool bar methods==============================
 
     private void setToolBar() {
          toolbar=(Toolbar)findViewById(R.id.toolbar);
@@ -99,6 +105,9 @@ public class MainActivity extends AppCompatActivity {
         drawerLayout.setDrawerListener(actionBarDrawerToggle);
 
     }
+
+
+    // ======================Tool bar methods==============================
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
@@ -121,6 +130,38 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(),"You select Setting",Toast.LENGTH_LONG).show();
         }
         return true;
+    }
+
+    private class EarthquakeAsyncTask extends AsyncTask<String, Void, String> {
+
+        /**
+         * This method is invoked (or called) on a background thread, so we can perform
+         * long-running operations like making a network request.
+         * <p>
+         * It is NOT okay to update the UI from a background thread, so we just return an
+         */
+        protected String doInBackground(String... urls) {
+            // Don't perform the request if there are no URLs, or the first URL is null.
+            if (urls.length < 1 || urls[0] == null) {
+                return null;
+            }
+
+            ProductResponseHandler p = new ProductResponseHandler();
+            String response = p.getProductJsonData(urls[0]);
+            ArrayList<Product> resultProductArray = p.getProductArray(response);
+            return response;
+        }
+
+        /**
+         * This method is invoked on the main UI thread after the background work has been
+         * completed.
+         * (which was returned from the doInBackground() method) and update the views on the screen.
+         */
+        protected void onPostExecute(String result) {
+            // If there is no result, do nothing.
+
+
+        }
     }
     // ====================================================
 }
--- app/src/main/java/com/example/android/jindalfresh/navigation/NavigationHelper.java
+++
@@ -1,6 +0,0 @@
-package com.example.android.jindalfresh.navigation;
-
-public class NavigationHelper {
-
-
-}
