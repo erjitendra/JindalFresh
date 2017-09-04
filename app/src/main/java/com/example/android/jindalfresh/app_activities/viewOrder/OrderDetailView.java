e514f07afe451ad2093fff9aadcad08e7832b7e2
erjitendra <jitendrashahar@gmail.com>
2017-09-04 13:12:16 +0530

    JK: View Order Page

---
+++ app/src/main/java/com/example/android/jindalfresh/app_activities/viewOrder/OrderDetailView.java
@@ -0,0 +1,15 @@
+package com.example.android.jindalfresh.app_activities.viewOrder;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.android.jindalfresh.R;
+
+public class OrderDetailView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_detail_view);
+    }
+}
