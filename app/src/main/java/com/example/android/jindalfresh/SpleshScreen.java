2b53e16149a0d27bedc7ead07af7aab216491222
erjitendra <jitendrashahar@gmail.com>
2017-09-21 13:25:22 +0530

    JK:added logo to splesh Screen

--- app/src/main/java/com/example/android/jindalfresh/SpleshScreen.java
+++ app/src/main/java/com/example/android/jindalfresh/SpleshScreen.java
@@ -4,23 +4,26 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.example.android.jindalfresh.app_activities.MainActivity;
 
 public class SpleshScreen extends Activity {
     private static int SPLASH_TIME_OUT = 3000;
+    private ImageView imageLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splesh_screen);
+        imageLogo = (ImageView) findViewById(R.id.img_jf_logo);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.splash_transition);
+        imageLogo.startAnimation(animation);
 
         new Handler().postDelayed(new Runnable() {
 
-            /*
-             * Showing splash screen with a timer. This will be useful when you
-             * want to show case your app logo / company
-             */
 
             @Override
             public void run() {
@@ -29,7 +32,7 @@ public class SpleshScreen extends Activity {
                 Intent i = new Intent(SpleshScreen.this, MainActivity.class);
                 startActivity(i);
 
-                // close this activity
+
                 finish();
             }
         }, SPLASH_TIME_OUT);
